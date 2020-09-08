8c50b4f551c7c6f0bfe90f68d4338a800ca14d8b
benjitrix <65090068+benjitrix@users.noreply.github.com>
2020-09-08 15:03:55 +0000

    Updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,17 +1,17 @@
 module.exports = {
   google: {
     clientID:
-      "420666988303-649msd0n7q2l6v4hq4n3lflq9jlgo0jo.apps.googleusercontent.com",
-    clientSecret: "jI73dU8VxDn4_VCsNiwDvZ7G",
+      "*****.apps.googleusercontent.com",
+    clientSecret: "*****",
   },
   twitter: {
-    consumerKey: "mGM5ojTuuApHTsCLabTauspzX",
-    consumerSecret: "QWlbWp4N3T14dMaTM4yxlFmlfVfdvwdIljoK1KVU6sMrX5UHb1",
-    accessToken: "27949113-1Is2tSpKzTtxjsKLCk490Pw1xdXM712nA4Rfn74vE",
-    accessTokenSecret: "9rJReSwkKcTMeEpvVN8VlYeFuTTgp3kSnj86CtRlSTPL5",
+    consumerKey: "*****",
+    consumerSecret: "*****,
+    accessToken: "*****",
+    accessTokenSecret: "*****",
     callbackURL: "http://localhost:5000/user/auth/twitter/callback",
   },
   mongoURI:
-    "mongodb+srv://ben:ben@mongodbtutotrial.wbkft.mongodb.net/reservation?retryWrites=true&w=majority",
+    "******",
   jwtSecret: "okechukwu",
 };
